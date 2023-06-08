a8095ab9f6472d84d523122b8f17a1556d9b9388
saurabh.kumar2@in.pega.com <saurabh.kumar2@in.pega.com>
2023-06-08 16:17:43 +0530

    BUG-801920 - changes to test all the files in one

--- validatexml.sh
+++ validatexml.sh
@@ -1,7 +1,35 @@
-if xmllint --output '{}' --format '{}' *.xml* > /dev/null 2>&1;
+if ne xmllint --noout charts/pega/config/deploy/context.xml.tmpl > /dev/null 2>&1;
 then
-   echo "XML formatting is correct"
-else
-  echo "XML formatting is incorrect"
-  exit 1
+    echo "context.xml.tmpl formatting is incorrect"
+    exit 1
+fi
+
+if ne xmllint --noout charts/pega/config/deploy/server.xml.tmpl > /dev/null 2>&1;
+then
+    echo "server.xml.tmpl formatting is incorrect"
+    exit 1
+fi
+
+if ne xmllint --noout charts/pega/config/deploy/prconfig.xml > /dev/null 2>&1;
+then
+    echo "deploy prconfig.xml formatting is incorrect"
+    exit 1
+fi
+
+if ne xmllint --noout charts/pega/config/deploy/prlog4j2.xml > /dev/null 2>&1;
+then
+    echo "deploy prlog4j2.xml formatting is incorrect"
+    exit 1
+fi
+
+if ne xmllint --noout charts/pega/charts/installer/config/prlog4j2.xml > /dev/null 2>&1;
+then
+    echo "installer prlog4j2.xml formatting is incorrect"
+    exit 1
+fi
+
+if ne xmllint --noout charts/pega/charts/installer/config/prconfig.xml.tmpl > /dev/null 2>&1;
+then
+    echo "installer prconfig.xml.tmpl formatting is incorrect"
+    exit 1
 fi
