71e628eadddf5b47d04c05afa861fc51139c05f1
saurabh.kumar2@in.pega.com <saurabh.kumar2@in.pega.com>
2023-06-08 15:03:38 +0530

    BUG-801920 - changes to xml lint

--- validatexml.sh
+++ validatexml.sh
@@ -1,4 +1,4 @@
-if xmllint --output '{}' --format '{}' --path 'charts/pega/config/deploy/context.xml.tmpl','charts/pega/config/deploy/server.xml.tmpl','charts/pega/config/deploy/prlog4j2.xml','charts/pega/config/deploy/prconfig.xml','charts/pega/charts/installer/config/prlog4j2.xml','charts/pega/charts/installer/config/prconfig.xml.tmpl' > /dev/null 2>&1;
+if xmllint --output '{}' --format '{}' *.xml* > /dev/null 2>&1;
 then
    echo "XML formatting is correct"
 else
